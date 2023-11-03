f5d5f61570042503fa54813b20bbf416b57286ab
OdenCraftsman <oden13123@gmail.com>
2023-11-04 03:32:17 +0900

    feat test_make_pdfの微調整
    ファイルを指定して、コンバートできるように変更

--- src/test/make_pdf.py
+++ src/test/make_pdf.py
@@ -6,9 +6,11 @@ from docx2pdf import convert
 if __name__ == "__main__":
     file_name = os.path.splitext(__file__.split("\\")[-1])[0]
 
-    doc = docx.Document()
+    converted_file_name = f"../../test_output/make_table.docx"
 
-    doc.add_paragraph("Hello world!")
+    doc = docx.Document(converted_file_name)
 
-    doc.save(f"../../test_output/{file_name}.docx")
-    convert(f"../../test_output/{file_name}.docx", f"../../test_output/{file_name}.pdf")
+    # doc.add_paragraph("Hello world!")
+    # doc.save(f"../../test_output/{file_name}.docx")
+
+    convert(converted_file_name, f"../../test_output/{file_name}.pdf")
